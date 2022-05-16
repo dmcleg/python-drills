09ec6f052d9ee5a4be7c42dabeba08a6418f0310
dmcleg <dmcleghorn@gmail.com>
2022-05-16 15:28:54 -0500

    Completed more python syntax drills.

--- drills.py
+++ drills.py
@@ -1,12 +1,28 @@
 '''
+    Function that applies \n and \t to Twinkle Twinkle Little Star.
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n")
 
+
+    Function that declares current python version.
 import sys
 print("User Current Version:-", sys.version)
 
-
+    Function that declares current date and time.
 import datetime
 now = datetime.datetime.now()
 print("\n\nCurrent date & time:\n", now.strftime("%Y-%m-%d %H:%M:%S"))
 '''
+'''
+    Function that determined the area of a circle using the radius.
 
+def radius(num):
+    return 3.14*(num*num)
+print(radius(12))
+'''
+'''
+    Function that takes a first and last name and swaps them.
+
+def namer(str, string):
+    return string + " " + str
+print(namer("Drew", "Cleghorn"))
+'''
