628f0390adf3996380c7596c9ce0878e4b754a8c
dmcleg <dmcleghorn@gmail.com>
2022-05-16 10:34:52 -0500

    First round of Python syntax drills.

---
+++ drills.py
@@ -0,0 +1,12 @@
+'''
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n")
+
+import sys
+print("User Current Version:-", sys.version)
+
+
+import datetime
+now = datetime.datetime.now()
+print("\n\nCurrent date & time:\n", now.strftime("%Y-%m-%d %H:%M:%S"))
+'''
+
